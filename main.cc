79b93431b6f02aedcc991ddfddbbd8329daf817b
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-04-11 11:17:06 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,6 +6,21 @@ using namespace std;
 
 int main()
 {
-
-
+    cout << "Welcome to the RPG" << endl; 
+    
+  while (true) {
+  int xCoor = 0;
+  int yCoor = 0;
+  char c; 
+  while (true) {
+    cin >> c; 
+  if (c == 'a') { xCoor -= 1; }
+    else if (c == 's') { yCoor -= 1; }
+      else if (c == 'd') { xCoor += 1; }
+        else if (c == 'w') { yCoor += 1; }
+      cout << "X coordinate =" << xCoor << endl<< "Y coordinate =" << yCoor << endl;
+               }
+  
+  
+}
 }
