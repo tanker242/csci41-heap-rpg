ae6cdd2563a4a32e889c5d0a216b990976409a94
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-04-16 03:01:19 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cstdlib>
-
-
+#include "movement.h"
+#include "heap.h"
+#include "dialogue.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Welcome to the RPG" << endl << "Use the ASWD keys to navigate the map" << endl; 
-    movement(); 
+int main() {
+    cout << "Welcome to the RPG" << endl << "Use the ASWD keys to navigate the map" << endl;
+    for (int i = 0; i < 10 ; i++) {
+        movement();
+        dialogue(i);
+    }
+    cout << "game over" << endl;
+
+
 }
