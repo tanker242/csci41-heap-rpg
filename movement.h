0aa1e71b769554b2fca53f21ad9e585e6ec9ab71
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-04-16 04:13:48 -0700

    Update movement.h

--- movement.h
+++ movement.h
@@ -14,17 +14,23 @@ void movement() {
         while (true) {
             cin >> c;
             if (c == 'a') {
+                cout << "One step to the left..." << endl; 
                 xCoor -= 1;
             } else if (c == 's') {
                 yCoor -= 1;
+                cout << "One step backwards" << endl;
             } else if (c == 'd') {
                 xCoor += 1;
+                cout << "one step to the right..." << endl;
             } else if (c == 'w') {
                 yCoor += 1;
+                cout << "one step forward!" << endl;
             }
-            cout << "X coordinate =" << xCoor << endl << "Y coordinate =" << yCoor << endl;
             count++;
-            if (count % 10 == 0) break;
+            if (count % 10 == 0){ 
+            cout << "X coordinate =" << xCoor << endl << "Y coordinate =" << yCoor << endl;
+            break;
+            }
         }
 
 
