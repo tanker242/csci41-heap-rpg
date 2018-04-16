ae9ead8605de107d68afe3556f9171d960e28c7b
tanker242 <38202670+tanker242@users.noreply.github.com>
2018-04-15 17:22:39 -0700

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -1,38 +1,35 @@
 //Start class prototypes- will actually be in an implementation file later today
 //Message me in discord if you have more to add or have suggestions
 
-class character {
+class Entity {
 
-static NPC = 0;
-static player = 0;
-
-protected:
+public:
+  //Variables
 int hp = 0;
 int mp = 0;
 int xp = 0;
-int level = 1;
-double speed = 0; // might end up being an int depending on heap
+int lvl = 1;
+int speed = 0; // might end up being an int depending on heap
 int str = 0; // stat will increase HP and warrior melee
-int mind = 0; // stat increases MP and spell damage
+int intel = 0; // stat increases MP and spell damage
 int agi = 0; // stat increases speed and agile attack damage
 bool isDead = 0;
-bool isAlly
+bool isAlly = 0;
 int row = 0 //Front row = 0 and back row = 1
 
-
-public:
-
+  //Methods
 void setHP ();
 int getHP ();
 void setMP ();
 int getMP ();
-int getLevel ();
+void setLvl ();
+int getLvl ();
 void setSpeed ();
-double getSpeed ();
+int getSpeed ();
 void setStr ();
 int getStr ();
-void setMind ();
-int getMind ();
+void setIntel ();
+int getIntel ();
 void setAgi ();
 int getAgi ();
 void set_isDead ();
@@ -46,7 +43,7 @@ int getRow ();
 
 //Start player classes
 
-class warrior:character :{
+class Warrior : public Entity :{
 
 protected:
 
@@ -54,7 +51,7 @@ public:
 
 };
 
-class rogue:character {
+class Rogue : public Entity {
 
 protected:
 
@@ -62,7 +59,7 @@ public:
 
 };
 
-class mage:character {
+class Mage : public Entity {
 
 protected:
 
@@ -70,7 +67,7 @@ public:
 
 };
 
-class cleric:character :{
+class Cleric : public character :{
 
 protected:
 
