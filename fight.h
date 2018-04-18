e1a33e5d79b25121cac6df22c301fc8483ed10c3
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-04-17 23:38:18 -0700

    Update fight.h

--- fight.h
+++ fight.h
@@ -17,32 +17,39 @@ void fightMove (){
             cout << line<< endl;
         }
     }
-
-
-
 }
 
 
-
-
-void fight (int fight){
+void fight(){
     int health = 100;
-while (health > 0){
-    if (fight == 0){
-    cout << "Ran from the fight" << endl;
-    break;
+    int opponent = rand() % 100;
+while (true) {
+        cout << "enter a number to continue" << endl << endl;
+        int y = 0;
+        cin >> y;
+        cout << "YOU used " ;
+        fightMove();
+        int attack = 0;
+            attack = rand() % 100;
+        cout << "   and dealt  " << attack << "  damage to your opponent" << endl << endl << endl;
+            opponent = opponent - attack;
+        cout << "Your opponents health is  " << opponent << endl << endl;
+         if (opponent <= 0){
+    cout << "your opponent has died" << endl;
+        break;
     }
-    else if ( fight != 0) {
-    cout << "choose a number between 1-10" << endl;
-    cout << "you used " ;
-    fightMove();
-    cout << "and dealt" << rand() % 100 << "damage" << endl;
-    health
-    cout << "Your opponent used " ;
-    fightMove();
-    cout << "and dealt" << (rand() % 100) << "damage" << endl;
 
 
+        cout << "Your opponent used " ;
+        fightMove();
+            int blow = 0;
+                blow = rand() %100;
+        cout << "   and dealt  " << blow << "  damage" << endl << endl << endl;
+            health = health - blow;
+        cout << "Your health is" << health << endl << endl;
+            if (health <= 0) {
+    cout << "Your health = 0 and therefore you have died, please try again" << endl;
+        exit(1);
     }
-}
+        }
 }
